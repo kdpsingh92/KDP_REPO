d483c434136b50e014c2136bb8a148c763428d2d
Kuldeep Singh <kdpsingh1992@gmail.com>
2018-09-23 20:20:12 +0530

    kuldeep to kuldeep_Singh changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld
 {
     public static void main(String args[])
     {
-    	System.out.println("Welcom to the java world Mr. Kuldeep");
+    	System.out.println("Welcom to the java world Mr. Kuldeep_Singh");
     }
 
 
